c155133c4a443a8702f68bcfc495a66c6b1df677
Cong Le <cle155@csu.fullerton.edu>
2025-04-29 02:10:21 -0700

    initial setup

---
+++ HarmonicShaders/HarmonicShaders/HarmonicShadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonicShadersApp.swift
+//  HarmonicShaders
+//
+//  Created by Cong Le on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct HarmonicShadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
